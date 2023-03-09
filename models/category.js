e6a266040f920d55a93728e5e96c7b3c6cca366b
Stonehagen <steinhagen@mailbox.org>
2023-03-09 15:30:30 +0100

    add Item and Category models

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, require: true, maxLength: 1000 },
+});
+
+CategorySchema.virtual('url').get(function getUrl() {
+  // eslint-disable-next-line no-underscore-dangle
+  return `/category/${this._id}`;
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { tpe: String, required: true, maxLength: 2500 },
+  category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
+  price: {
+    type: Number,
+    required: true,
+    min: [0, 'Price must be higher 0 or higher'],
+    max: [1000000, 'Price must be 1000000 or lower'],
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: [0, 'Stock must be higher 0 or higher'],
+    max: [100000000, 'Stock must be 100000000 or lower'],
+  },
+});
+
+ItemSchema.virtual('url').get(function getUrl() {
+  // eslint-disable-next-line no-underscore-dangle
+  return `/item/${this._id}`;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
